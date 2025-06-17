a75e8a644bbfa715270a552dfc69246e376bc382
DaysiMassielka <54219496+DaysiMassielka@users.noreply.github.com>
2025-06-17 07:37:05 +0000

    Generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-import "bootstrap";
-import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+
+  //write your code here
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function generateExcuse() {
+ let randoWho = who[Math.floor(Math.random() * who.length)];
+ let randoAction = action[Math.floor(Math.random() * action.length)];
+ let randoWhat = what[Math.floor(Math.random() * what.length)];
+ let randoWhen = when[Math.floor(Math.random() * when.length)];
+
+ const excuse = `${randoWho} ${randoAction} ${randoWhat} ${randoWhen}`;
+
+ document.getElementById('excusa').innerHTML = excuse;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateExcuse();
+
+
+const boton = document.getElementById('generarBtn');
+
+boton.addEventListener('click', generateExcuse);
+
 };
